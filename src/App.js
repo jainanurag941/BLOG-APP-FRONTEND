ef354a6d1441acd97ce4380e403e12a5ee139c49
Anurag Jain <jainanurag941@gmail.com>
2022-12-12 18:05:41 +0530

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
