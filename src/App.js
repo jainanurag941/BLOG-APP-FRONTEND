2f77eccd5bffa99dac0aaf0075a40f60920b9ad8
Anurag Jain <jainanurag941@gmail.com>
2022-12-12 19:50:08 +0530

    User Registration Action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import Register from "./components/Users/Register/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/redux/slices/users/usersSlices.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//register action
+export const registerUserAction = createAsyncThunk(
+  "users/register",
+  async (user, { rejectWithValue, getState, dispatch }) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "http://localhost:5000/api/users/register",
+        user,
+        config
+      );
+      return data;
+    } catch (error) {
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
