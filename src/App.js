aba3846ba2657934fbbf7f0291ef0d6d764a6df9
Anurag Jain <jainanurag941@gmail.com>
2022-12-13 00:25:54 +0530

    User Login Form Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import Login from "./components/Users/Login/Login";
 import Register from "./components/Users/Register/Register";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </BrowserRouter>
   );
